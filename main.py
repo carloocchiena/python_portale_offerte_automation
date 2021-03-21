9ab81fa8deb9e155f9ce8c4bf9042cc5e026b36c
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-03-21 19:07:17 +0100

    Update main.py
    added user data fields for gas or power or dual

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ import time #to pause the scrolling
 #load the chrome webdriver (get the right one from https://chromedriver.chromium.org/downloads)
 browser = webdriver.Chrome("C:\YOURPATH\chromedriver.exe")
 
-#user data
+#user data (enter here the data you need to modify)
 user_cap = "20154"
 power = 2700
 gas = 1000
 url = "https://www.ilportaleofferte.it/portaleOfferte/it/confronto-tariffe-prezzi-luce-gas.page?tipoOfferta=&code-istat=&cap-comune="
+contract_type = "power" 
+#use: "gas" "dual" or "power"
 
-#login into the website
+#get into the website
 browser.get(url)
+
 #page1
-dual = browser.find_element_by_id("dual").click()
+
+if contract_type == "dual": 
+    dual = browser.find_element_by_id("dual").click()
+elif contract_type == "gas":
+    gas_only = browser.find_element_by_id("gas").click()
+else:
+    power_only = browser.find_element_by_id("energiaElettrica").click()
+
 
 cap = browser.find_element_by_id("capComune")
 cap.send_keys(user_cap)
@@ -26,29 +36,48 @@ pfix = browser.find_element_by_id("prezzoFisso").click()
 
 casa = browser.find_element_by_id("casa").click()
 
-residente = browser.find_element_by_id("residenteSI").click()
+if contract_type == "dual" or contract_type == "power":
+    residente = browser.find_element_by_id("residenteSI").click()
+
+else:
+    pass 
 
 avanti = browser.find_element_by_class_name("btn").click()
 
 #page2
-potenza =  Select(browser.find_element_by_id("scegliPotenza"))
-potenza.select_by_index(2)
 
-consumo_luce = browser.find_element_by_id("consumoSI").click()
+if contract_type == "dual" or contract_type == "power":
+    potenza =  Select(browser.find_element_by_id("scegliPotenza"))
+    potenza.select_by_index(2)
 
-kwh = browser.find_element_by_id("consumoAnnuoSomma")
-kwh.send_keys(power)
+    consumo_luce = browser.find_element_by_id("consumoSI").click()
 
-avanti = browser.find_element_by_link_text("Avanti").click()
+    kwh = browser.find_element_by_id("consumoAnnuoSomma")
+    kwh.send_keys(power)
+    
+    try:
+        avanti = browser.find_element_by_link_text("Avanti").click()
+    except:
+        confronta = browser.find_element_by_name("confronta").click()
+
+else:
+    pass
 
 #page3
-cottura = browser.find_element_by_xpath('//label[@for="cottura"]').click()
-acqua = browser.find_element_by_xpath('//label[@for="acquaCalda"]').click()
-riscaldamento = browser.find_element_by_xpath('//label[@for="riscaldamento"]').click()
 
-consumo_gas = browser.find_element_by_id("consumoGasSI").click()
+if contract_type == "dual" or contract_type == "gas":
+    cottura = browser.find_element_by_xpath('//label[@for="cottura"]').click()
+    acqua = browser.find_element_by_xpath('//label[@for="acquaCalda"]').click()
+    riscaldamento = browser.find_element_by_xpath('//label[@for="riscaldamento"]').click()
 
-smc = browser.find_element_by_id("ConsumoAnnuoGas")
-smc.send_keys(gas)
+    consumo_gas = browser.find_element_by_id("consumoGasSI").click()
 
-avanti = browser.find_element_by_link_text("Avanti").click()
+    smc = browser.find_element_by_id("ConsumoAnnuoGas")
+    smc.send_keys(gas)
+
+else:
+    pass 
+try:
+    avanti = browser.find_element_by_link_text("Avanti").click()
+except:
+    confronta = browser.find_element_by_name("confronta").click()
