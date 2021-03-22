6f690d85336789b1d51799af0cd135cd6e69f02e
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-03-22 13:57:59 +0100

    Update main.py
    updated for results extractions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup as BS4   #for handling xml page
+from datetime import date #today's date
+import lxml #to decode xml page with bs4
 from selenium import webdriver   #to handle chromium webdriver
 from selenium.webdriver.common.keys import Keys #to have keys name
 from selenium.webdriver.support.ui import Select #to select from dropdown list
@@ -10,8 +13,13 @@ browser = webdriver.Chrome("C:\YOURPATH\chromedriver.exe")
 user_cap = "20154"
 power = 2700
 gas = 1000
-contract_type = "power" 
+url = "https://www.ilportaleofferte.it/portaleOfferte/it/confronto-tariffe-prezzi-luce-gas.page?tipoOfferta=&code-istat=&cap-comune="
+contract_type = "dual" 
 #use: "gas" "dual" or "power"
+
+#global
+rank = 1
+today = date.today()
 
 url = "https://www.ilportaleofferte.it/portaleOfferte/it/confronto-tariffe-prezzi-luce-gas.page?tipoOfferta=&code-istat=&cap-comune="
 
@@ -79,3 +87,16 @@ try:
     avanti = browser.find_element_by_link_text("Avanti").click()
 except:
     confronta = browser.find_element_by_name("confronta").click()
+    
+    
+#printing out the results: 
+
+new_url = browser.page_source
+soup = BS4(new_url, "lxml")
+competitors = soup.find_all("p", {"class": "nome_venditore"})
+
+for comp in competitors:
+    print (str(today)+ " " + str(rank)+" :"+comp.text)
+    rank += 1
+    
+rank = 1 #reset the counter in case of multiple runs
